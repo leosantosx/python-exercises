d5a493b2d17dc5df8affd86d2af8ed66d86d4b85
Leonardo Santos <leonardosanttos0077@gmail.com>
2020-09-23 20:55:04 -0300

    atualizando exercícios

--- Orientação a Objetos/Resolução dos exercícios/ex_02/test.py
+++ Orientação a Objetos/Resolução dos exercícios/ex_02/test.py
@@ -1,23 +1,3 @@
-"""
-Exercício 02
-Crie uma classe para representar uma pessoa, com os atributos nome, idade, altura e peso.
-essa classe deve ter um metódo que imprime todos os dados e outro metódo que calcula o IMC (Índice de massa corporal)
-
-Ela deve funcionar dessa forma:
-
-from pessoa import Pessoa
-p = Pessoa('Leo', 19, 1.69, 70)
-p.imprime_dados()
-p.calcula_imc()
-
-Imprime:
-        >> Leo tem 19 anos, 1.69 de altura e pesa 70kg.
-        >> Seu Imc é 24.51
-
-Mãos à obra!
-
-"""
-
 from pessoa import Pessoa
 
 p = Pessoa('Leo', 19, 1.69, 70)
